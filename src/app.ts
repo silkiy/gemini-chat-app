2c08772e00f45f5250f1246a8dd9b9239795e409
Silki <129131071+silkiy@users.noreply.github.com>
2025-07-27 18:51:37 +0700

    feat: enhance app structure with improved error handling and server setup

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,38 @@
-import express from "express";
-import "dotenv/config";
-import chatRoutes from "./routers/chat.routes";
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import router from "./routers/chat.routes";
+import { createServer } from "http";
+import { parse } from "url";
+
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use("/api", chatRoutes);
+app.use("/api", router);
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
+app.use((req: Request, res: Response) => {
+  res.status(404).json({
+    message: "NOT FOUND",
+    code: 404,
+  });
 });
+
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+}
+
+export default async function handler(req: any, res: any) {
+  const parsedUrl = parse(req.url!, true);
+  req.query = parsedUrl.query;
+
+  const server = createServer((req, res) => {
+    app(req as any, res as any);
+  });
+
+  server.emit("request", req, res);
+}
